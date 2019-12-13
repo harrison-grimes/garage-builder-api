a02c09def15104d7932e2587c43131edeb47f31b
Harrison <hagrim@gmail.com>
2019-12-13 15:00:27 -0700

    connect mongoDB URI

---
+++ db/keys.js
@@ -0,0 +1,3 @@
+module.exports {
+  mongoURI: 'mongodb+srv://harry:harry123@cluster0-bdofu.mongodb.net/test?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // initialize environment values
-require('dotenv').config();
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3007;
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config();
+}
 
 // 3rd party dependencies
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // internal dependencies
 const api = require('./routes');
 
 // create express app
 const app = express();
+
+//DB config
+const db = require(./db/keys).mongoURI;
+
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // express middleware
 app.use(bodyParser.json());
